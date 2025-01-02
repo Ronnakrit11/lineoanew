cf7cce4c036b3f05d5a002879683c4de9d0df0a5
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-02 23:43:20 +0700

    done cloundinary

--- app/components/line-account/settings/LineAccountSettingsCard.tsx
+++ app/components/line-account/settings/LineAccountSettingsCard.tsx
@@ -81,8 +81,8 @@ export function LineAccountSettingsCard({ account, onUpdate }: LineAccountSettin
             className="flex items-center gap-2"
             onClick={() => setIsSettingsOpen(true)}
           >
-            <Settings className="w-4 h-4" />
-            <span>Edit</span>
+            <Settings className="w-4 h-4 " />
+            <span className='text-slate-500 '>Edit</span>
           </Button>
         </div>
       </div>
